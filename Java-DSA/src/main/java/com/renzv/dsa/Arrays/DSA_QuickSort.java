db530e235f57dd23d46276fe770f225c8d757bb2
renzv-compsci <viloriarenz917@gmail.com>
2025-07-30 23:50:17 +0800

    Initial commit for Quick Sort: Subject for review

---
+++ Java-DSA/src/main/java/com/renzv/dsa/Arrays/DSA_QuickSort.java
@@ -0,0 +1,59 @@
+package com.renzv.dsa.Arrays;
+
+public class DSA_QuickSort {
+    public static void main (String[] args) {
+
+        int[] array1  = {5, 56, 9, 3, 15, 78};
+        // Pass array1 as the array
+        // 0 as the startinn index (low)
+        // array1.length - 1; 5 as the ending index (high)
+        // E.g Simulation: quicksort(array1, 0, 5)
+        quicksort(array1, 0, array1.length - 1); // Calls the quicksort on the whole array (from index 0 to 5)
+
+        System.out.print("Sorted array: ");
+        for (int value : array1) {
+            System.out.print(value + ", ");
+        }
+    }
+    // Defines the recursive quicksort method with passing parameters 
+    // array, the array to sort
+    // low, the starting index 
+    // high, the ending index 
+    public static void quicksort(int[] array, int low, int high) {
+        // Checks if the current subarray has more than one element; if not the recursion stops 
+        if (low < high) { 
+
+            // partition(array1, 0, 5) executes here 
+            int pivotIndex = partition(array, low, high); // This calls the partition method to rearrang the subarray and find the pivots final index
+            // And returns the pivot index in pivotIndex 
+            quicksort(array, low, pivotIndex -1); // Sorts the left subarray before the pivot 
+            quicksort(array, pivotIndex + 1, high); // Sorts the right subarray after pivot 
+        }
+    }
+
+    // Defines partition method 
+    // Receives the array and the current bounds low and high 
+    public static int partition(int array[], int low, int high) {
+        int pivot = array [high]; // It selects the last element in the current subarray as the pivot 
+        int i = low -1; // Initializes the i to one less than low; this marks the wall between smaller and larger elements 
+
+        // Loops through the subarray from low to high - 1 with index j 
+        for (int j = low; j < high; j++) { // Checks if the current element is less than or equal to the pivot 
+            if (array[j] <= pivot) {
+                i++;
+
+                // Swaps the element at j with element at i
+                // Putting smaller elements to the left of the wall 
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        // Swaps at pivot element high with the element right after the last smallest element i + 1 
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1; // Returns the index of the pivot elements 
+    }
+} 
