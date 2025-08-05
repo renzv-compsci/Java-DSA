f6a5186c607c4fb661235a31488e6cf4acc5cf5b
renzv-compsci <viloriarenz917@gmail.com>
2025-08-05 22:32:56 +0800

    Add practice implementation of counting sort algorithm

--- Java-DSA/src/main/java/com/renzv/dsa/Arrays/DSA_CountingSort.java
+++ Java-DSA/src/main/java/com/renzv/dsa/Arrays/DSA_CountingSort.java
@@ -14,26 +14,34 @@ public class DSA_CountingSort {
         System.out.println();
     }
 
+    // Static method that takes integer array as input 
     public static void countingSort(int[] array) {
-        int maxValue = array[0];
+        int maxValue = array[0]; // Initializes to the first value in the array 
         
-        for (int num : array) {
-            if(num > maxValue) {
+        // This findsa the maximum element in the array 
+        // Loop each num in array 
+        for (int num : array) { 
+            if(num > maxValue) { // If num is greater than maxValue, update maxValue 
                 maxValue = num;
             }
         }
 
-        int[] count = new int[maxValue + 1];
+        // Declares a new integera array count of size maxValue + 1
+        int[] count = new int[maxValue + 1]; // Stores the frequency of each integer value in array
+        // E.g if maxValue is 6 the count array will have 7 slots (0-6)
 
+        // Loops through each num in array 
         for (int num : array) {
             count[num]++;
         }
 
+        // Use to write sorted values back into the array 
         int index = 0;
         
+        // Loops through all possible values from i (0) to maxValue 
         for (int i = 0; i <= maxValue; i ++) {
             while(count[i] > 0) { 
-                array[index++] = i;
+                array[index++] = i; // Writes value to the array 
                 count[i]--;
             }
         }
